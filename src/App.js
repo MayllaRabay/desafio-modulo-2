899bd3521fc3ad1e85a3f23d85c409e3fcbc1b5f
MayllaRabay <mayllarabay@gmail.com>
2021-06-16 13:57:37 -0300

    Header parcialmente feito e estilizado.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import logoIcon from './assets/images/logo.svg';
+import searchIcon from './assets/images/search-icon.svg';
+import userIcon from './assets/images/user.png';
 import styles from './styles/App.module.scss';
 
 function App() {
   return (
     <div className={styles.main__container}>
-      <h1>Teste um dois três ...</h1>
+      <div className={styles.header__wrapper}>
+        <form>
+          <img src={logoIcon} alt="" />
+          <input placeholder="Pesquise filmes..." />
+          <img className={styles.search__icon} src={searchIcon} alt="" />
+        </form>
+        <div className={styles.user__info}>
+          <span>Bem vindo Link!</span>
+          <img src={userIcon} alt="" width="50px"/>
+        </div>
+      </div>
+
+      <div className={styles.top__films__wrapper}>
+        <h1>Top filmes</h1>
+        <div className={styles.top__films__cards}>
+          <div className={styles.film__card}>
+            <img src="" alt="" />
+            <div className={styles.film__info}>
+              
+            </div>
+            <button></button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
